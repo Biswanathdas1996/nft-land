b59da9cfae411890150ac0ef3c37139510811b19
your_username <your_email_address@example.com>
2022-08-15 16:59:34 +0530

    ok

--- src/components/DetailsPage/PrivetContent.js
+++ src/components/DetailsPage/PrivetContent.js
@@ -1,38 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Typography, Stack } from "@mui/material";
+
 import { TabPanel } from "@mui/lab";
-import SpeakerNotesOffIcon from "@mui/icons-material/SpeakerNotesOff";
-import { _fetch, _account } from "../../CONTRACT-ABI/connect";
-import { Card, Grid } from "@mui/material";
-import CardActionArea from "@material-ui/core/CardActionArea";
 
-import CardMedia from "@material-ui/core/CardMedia";
-
-import FileImg from "../../assets/images/file.png";
-
-import Button from "@mui/material/Button";
-import GetAppIcon from "@material-ui/icons/GetApp";
-import CardActions from "@material-ui/core/CardActions";
+import { _account } from "../../CONTRACT-ABI/connect";
 
 import Certificate from "../../Pages/Certificate";
 
 const Bid = ({ tokenId }) => {
   const [account, setAccount] = useState([]);
-  const [start, setStart] = useState(false);
+
   useEffect(() => {
     fetchNftInfo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function fetchNftInfo() {
-    setStart(true);
     try {
       const getprivetContent = await _account();
       setAccount(getprivetContent);
-      setStart(false);
-    } catch {
-      setStart(false);
-    }
+    } catch {}
   }
 
   return (
