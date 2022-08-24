a2c0576a083df8cc41617702558676afdcdb90bd
Biswanath Das <cv.biswanath@gmail.com>
2022-08-24 12:42:14 +0530

    inio

--- src/utils/isAdmin.js
+++ src/utils/isAdmin.js
@@ -1,7 +1,7 @@
-const adminAccount = "0xb78b8053B701a8e83bE191431533090A51fbF7ED";
+const adminAccount = ["0xb78b8053B701a8e83bE191431533090A51fbF7ED","0x9A135C4d43b9fc6c4d5669d29e6442D7702F841c"];
 
 export function isAdmin(currentAccount) {
-  if (adminAccount === currentAccount) {
+  if (adminAccount.includes(currentAccount)) {
     return true;
   } else {
     return false;
